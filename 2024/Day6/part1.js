2191c68c6695e4193541cd2d8d684372da1d654e
Paul <ppd1512@gmail.com>
2025-03-11 18:17:23 +0000

    Created day 5 and 6 but havent worked on them

---
+++ 2024/Day6/part1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const { getFilePath } = require("../../utils");
+
+const filePath = getFilePath("/2024/Day6/input.txt");
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  countPositions(data);
+});
+
+const countPositions = (data) => {
+  const lines = data.split("\r\n");
+
+  lines.forEach((line, row) => {
+    console.log(line);
+  });
+};
+
+//todo finish it
